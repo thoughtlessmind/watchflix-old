85e47d1fb450e911ad79c5e4f38d8a0d0f16037d
Rajiv <1998irajeev@gmail.com>
2021-05-23 00:31:22 +0530

    welcome section with search input added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TrendingSectionComponent } from './trending-section/trending-section.component';
+import { WelcomeSectionComponent } from './welcome-section/welcome-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TrendingSectionComponent
+    TrendingSectionComponent,
+    WelcomeSectionComponent
   ],
   imports: [
     BrowserModule,
