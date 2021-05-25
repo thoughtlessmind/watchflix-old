5885e90c31214bfc242816c065afc1411368af84
Rajiv <1998irajeev@gmail.com>
2021-05-25 20:09:27 +0530

    scroll buttons added on trending list section

--- src/app/trending-section/trending-section.component.ts
+++ src/app/trending-section/trending-section.component.ts
@@ -7,17 +7,56 @@ import { TrendingService } from '../trending.service';
   styleUrls: ['./trending-section.component.css'],
 })
 export class TrendingSectionComponent implements OnInit {
-  trendingShows:any = [];
+  trendingShows: any = [];
 
   constructor(private trendingService: TrendingService) {}
 
   getTrending(): void {
     this.trendingService
       .getTrendings()
-      .subscribe((data) => this.trendingShows = data.results);
+      .subscribe((data) => (this.trendingShows = data.results));
+  }
+
+  manageScrollButtons(): void {
+    if(window.innerWidth < 600){
+      return
+    }
+    const el: HTMLElement | null = document.getElementById('scrollContainer');
+    const rightButtonEl = document.getElementById('rightScroll');
+    const lefttButtonEl = document.getElementById('leftScroll');
+    const leftPos: number = el?.scrollLeft || 0;
+    const elWidth: number = el?.getBoundingClientRect()?.width || 0;
+
+
+    if (leftPos > 0) {
+      lefttButtonEl?.classList.remove('hide');
+    } else {
+      lefttButtonEl?.classList.add('hide');
+    }
+
+    if (leftPos >= elWidth * 2) {
+      rightButtonEl?.classList.add('hide');
+    } else {
+      rightButtonEl?.classList.remove('hide');
+    }
+  }
+
+  scrollRight(direction: string): void {
+    const el: HTMLElement | null = document.getElementById('scrollContainer');
+    if (direction === 'left') {
+      el?.scroll({
+        left: el.scrollLeft - 180,
+        behavior: 'smooth',
+      });
+    } else {
+      el?.scroll({
+        left: el.scrollLeft + 180,
+        behavior: 'smooth',
+      });
+    }
   }
 
   ngOnInit() {
-    this.getTrending()
+    this.getTrending();
   }
 }
